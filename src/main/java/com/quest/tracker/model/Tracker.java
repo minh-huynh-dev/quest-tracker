b6bc3de8a16425c3db61d720f109b25c92ea2b27
Vic <kim.victor.17@gmail.com>
2021-05-13 00:33:05 -0400

    Percentages
    Calculated percentages for daily and weekly quests

--- src/main/java/com/quest/tracker/model/Tracker.java
+++ src/main/java/com/quest/tracker/model/Tracker.java
@@ -3,21 +3,25 @@ package com.quest.tracker.model;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 @Component
 @Data
-public class Calendar {
+public class Tracker {
 
-    List<Quest> quest;
+    List<Quest> dailies;
+    List<Quest> weeklies;
+    Calendar calendar;
 
     @Component
     @Data
-    private class Quest {
+    public class Quest {
         private String name;
         private Date expirationDate;
         private Boolean isWeekly;
+        private Boolean isComplete;
     }
 
 }
